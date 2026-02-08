19b5f34249fab782662b9aae68d87e8b1a4dccbf
fbruschetti <fbruschetti@alteanet.it>
2026-02-08 13:30:28 +0100

    Built web
    Branch: test
    Commit: 6849d5dd607c4fb6caf182cb7442cb4dc3fadec0
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "a89774184abe5a3e5c5acab3f86ce1f8",
+"flutter_bootstrap.js": "5ce42bc9ee646c5e4bf630a34d09506d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
