a11bf25f1b16e7bf58ffba68caa790b9205a09fa
fbruschetti <fbruschetti@alteanet.it>
2026-05-03 14:08:49 +0200

    Built web
    Branch: dev
    Commit: 426f5d20ec0238976a05383f121d16d65137096c
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3722727682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1113641719" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
