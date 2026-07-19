4225a292dfa49840485f709bc861513049c32af4
fbruschetti <francesco.bruschetti@yahoo.it>
2026-07-19 18:07:40 +0200

    [FIX] Fixed ad-expense edge function

--- supabase/functions/add-expense/index.ts
+++ supabase/functions/add-expense/index.ts
@@ -43,8 +43,8 @@ Deno.serve(async (req) => {
     const { error } = await supabase.rpc('insert_group_transaction_with_merchant_category', {
       'p_group_id': body.groupId,
       'p_user_id': auth.userId,
-      'p_paid_amount': null, // paidAmount,
-      'p_total_amount': 10.10, // price,
+      'p_paid_amount': paidAmount,
+      'p_total_amount': price,
       'p_split_rate': splitRateEnum,
       'p_merchant_name': body.merchant ?? null,
       'p_category_name': body.categories ?? null,
